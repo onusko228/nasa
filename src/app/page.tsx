6f06648ac83e7de503abc4fb08e207e6982580fd
onusko228 <alex2312cool@gmail.com>
2025-04-13 18:54:27 +0300

    fixed images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
   const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
   const [data, setData] = useState(null);
   const [error, setError] = useState('');
-  const [show,setShow]=useState(true);
+  const [showDescription, setShowDescription] = useState(true); // Новий стан для опису
   const API_KEY = process.env.NEXT_PUBLIC_NASA_API_KEY;
 
   const fetchData = useCallback(async (selectedDate: string) => {
@@ -28,27 +28,25 @@ export default function HomePage() {
 
   useEffect(() => {
     fetchData(date);
-  }, [date, fetchData]); 
+  }, [date, fetchData]);
 
   const handleDateChange = useCallback(() => {
     fetchData(date);
   }, [fetchData, date]);
 
-  const showNext = () => {
-    setShow(false);
-  }
+  const handleNext = () => {
+    setShowDescription(false);
+  };
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-900 text-white p-4">
-      {show ? (
+      {showDescription ? (
         <div className="max-w-2xl text-center">
           <p className="text-lg mb-4">
-          Щоденний космос — це веб-додаток,з використанням NASA API, який щодня відкриває перед вами красу Всесвіту через зображення від NASA.
- Переглядайте захопливі фото космосу з науковими поясненнями англійською мовою.
- Обирайте дату, щоб побачити, яке космічне диво було зафіксовано саме тоді.
+            Одним із найпопулярніших веб-сайтів NASA є Astronomy Picture of the Day. Фактично, цей веб-сайт є одним із найпопулярніших веб-сайтів у всіх федеральних агентств. Це популярне відео Джекі Бівера. Для кінцева точка структури зображення APOD і потенційно метадані, щоб їх можна було використовувати для різних програм. Крім того, якщо concept_tags для цього метадані встановлено значення True, повертаються ключові слова, отримані з пошуку зображень. Ключові слова можна використовувати як автоматичні хештеги для каналу Twitter або Instagram; якщо задано додаткові знання релевантні зображення.
           </p>
           <button
-            onClick={showNext}
+            onClick={handleNext}
             className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
           >
             Далі
